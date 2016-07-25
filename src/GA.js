8fdda830825b2725fd124be933d31664ba2dab7b
Elías Puma <eliasj.puma@gmail.com>
2016-07-25 08:29:43 -0500

    Outputs!

--- src/GA.js
+++ src/GA.js
@@ -1,3 +1,18 @@
-var hello = "Hello World";
+console.log("run:");
 
-console.log(hello);
+var pob_size = 4,
+    cromosoma_size = 5,
+    iterations = 30,
+    crossover_prob = 0.9,
+    crossover_point = 3,
+    mut_prob = 0.05;
+
+//Outputs
+console.log("Tamaño de la Población: " + pob_size);
+console.log("Tamaño de los Cromosomas: " + cromosoma_size);
+console.log("Cantidad de Iteraciones: " + iterations);
+console.log("Probabilidad de Cruzamiento: " + crossover_prob);
+console.log("Cruzamiento de un Punto - Punto " + crossover_point);
+console.log("Probabilidad de Mutación " + mut_prob);
+console.log("Mutación Simple"); //Dependerá de otra variable
+
